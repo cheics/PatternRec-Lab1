01a5ed71c998a1e325668857772b821c9be7b323
cheics <cheics@gmail.com>
2012-02-02 21:21:00 -0500

    Modifying template for pattern rec

--- matlabFiles/normalizeImage.m
+++
@@ -1,7 +0,0 @@
-function [ imgOut ] = normalizeImage( imgIn )
-%UNTITLED6 Summary of this function goes here
-%   Detailed explanation goes here
-    imgOut =(imgIn-min(min(imgIn)))./(max(max(imgIn))-min(min(imgIn)));
-
-end
-
--- matlabFiles/psnr.m
+++
@@ -1,8 +0,0 @@
-function [ outPsnr ] = psnr( f, g, maxVal)
-%psnr Peak Signal-to-noise ratio
-%   Operates on images that are [0,1]
-
-outPsnr=10*log10(maxVal/mean2((f-g).^2));
-
-end
-
