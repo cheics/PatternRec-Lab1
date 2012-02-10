eb39b8926bb6b6c6922a9e881d1a8be8359a782e
cheics <cheics@gmail.com>
2012-02-09 22:59:54 -0500

    Added NN class fix

--- matlabFiles/errorAnal.m
+++ matlabFiles/errorAnal.m
@@ -27,56 +27,30 @@ cc=confusionmat(dataSet_CDE(:,3),GED_results_CDE);
 dd=confusionmat(dataSet_CDE(:,3),MED_results_CDE);
 ee=confusionmat(dataSet_CDE(:,3),MAP_results_CDE);
 
-%% For nearest neighbor need to have training set...
-dataSet_AB_shuf=dataSet_AB(randperm(length(dataSet_AB)), :);
-dataSet_CDE_shuf=dataSet_CDE(randperm(length(dataSet_CDE)), :);
-
-%% use half as training...
-dataSet_AB_train=dataSet_AB_shuf(1:length(dataSet_AB)/2, :);
-dataSet_AB_test=dataSet_AB_shuf(length(dataSet_AB)/2:end, :);
-dataSet_CDE_train=dataSet_CDE_shuf(1:length(dataSet_CDE)/2, :);
-dataSet_CDE_test=dataSet_CDE_shuf(length(dataSet_CDE)/2:end, :);
-
-%% Establish classifiers
-train_NN_class_AB=knnclassify(xyGrid_list_AB, ...
-	dataSet_AB_train(:,1:2), ...
-	dataSet_AB_train(:,3) ...
-);
-train_NN_class_AB=reshape(train_NN_class_AB,size(MED_AB,1),size(MED_AB,2));
-
-train_NN_class_CDE=knnclassify(xyGrid_list_CDE, ...
-	dataSet_CDE_train(:,1:2), ...
-	dataSet_CDE_train(:,3) ...
-);
-train_NN_class_CDE=reshape(train_NN_class_CDE,size(MED_CDE,1),size(MED_CDE,2));
-
-train_NN5_class_AB=knnclassify(xyGrid_list_AB, ...
-	dataSet_AB_train(:,1:2), ...
-	dataSet_AB_train(:,3), 5 ...
-);
-train_NN5_class_AB=reshape(train_NN5_class_AB,size(MED_AB,1),size(MED_AB,2));
-
-train_NN5_class_CDE=knnclassify(xyGrid_list_CDE, ...
-	dataSet_CDE_train(:,1:2), ...
-	dataSet_CDE_train(:,3), 5 ...
-);
-train_NN5_class_CDE=reshape(train_NN5_class_CDE,size(MED_CDE,1),size(MED_CDE,2));
 
 %% Test
+A_test = gaussTransform(randn(N_C,2),mu_C,Sigma_A);
+B_test = gaussTransform(randn(N_D,2),mu_D,Sigma_B);
+C_test = gaussTransform(randn(N_C,2),mu_C,Sigma_C);
+D_test = gaussTransform(randn(N_D,2),mu_D,Sigma_D);
+E_test = gaussTransform(randn(N_E,2),mu_E,Sigma_E);
 
-NN_results_AB = griddata(xVals_AB,yVals_AB,train_NN_class_AB, ...
-	dataSet_AB_test(:,1),dataSet_AB_test(:,2), 'nearest');
-NN5_results_AB = griddata(xVals_AB,yVals_AB,train_NN5_class_AB, ...
-	dataSet_AB_test(:,1),dataSet_AB_test(:,2), 'nearest');
+nn_dataSet_AB=vertcat([A_test, ones(length(A_test),1)*1], [B_test, ones(length(B_test),1)*2]);
+nn_dataSet_CDE=vertcat([C_test, ones(length(C_test),1)*1], [D_test, ones(length(D_test),1)*2], [E_test, ones(length(E_test),1)*3]);
 
-NN_results_CDE = griddata(xVals_CDE,yVals_CDE,train_NN_class_CDE, ...
-	dataSet_CDE_test(:,1),dataSet_CDE_test(:,2), 'nearest');
-NN5_results_CDE = griddata(xVals_CDE,yVals_CDE,train_NN5_class_CDE, ...
-	dataSet_CDE_test(:,1),dataSet_CDE_test(:,2), 'nearest');
+NN_results_AB = griddata(xVals_AB,yVals_AB,NN_class_AB, ...
+	nn_dataSet_AB(:,1),nn_dataSet_AB(:,2), 'nearest');
+NN5_results_AB = griddata(xVals_AB,yVals_AB,NN5_class_AB, ...
+	nn_dataSet_AB(:,1),nn_dataSet_AB(:,2), 'nearest');
+
+NN_results_CDE = griddata(xVals_CDE,yVals_CDE,NN_class_CDE, ...
+	nn_dataSet_CDE(:,1),nn_dataSet_CDE(:,2), 'nearest');
+NN5_results_CDE = griddata(xVals_CDE,yVals_CDE,NN5_class_CDE, ...
+	nn_dataSet_CDE(:,1),nn_dataSet_CDE(:,2), 'nearest');
 
 %% CONF?
-confusionmat(dataSet_AB_test(:,3),NN_results_AB);
-confusionmat(dataSet_AB_test(:,3),NN5_results_AB);
-confusionmat(dataSet_CDE_test(:,3),NN_results_CDE);
-confusionmat(dataSet_CDE_test(:,3),NN5_results_CDE);
+confusionmat(nn_dataSet_AB(:,3),NN_results_AB)
+confusionmat(nn_dataSet_AB(:,3),NN5_results_AB)
+confusionmat(nn_dataSet_CDE(:,3),NN_results_CDE)
+confusionmat(nn_dataSet_CDE(:,3),NN5_results_CDE)
 
